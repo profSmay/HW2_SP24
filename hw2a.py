7e218e725bb054006db05740eb76d104b0e0a117
jimsmay <jim.smay@okstate.edu>
2024-02-01 12:13:59 -0600

    a more detailed plan of action

--- hw2a.py
+++ hw2a.py
@@ -1,15 +1,57 @@
 def Probability(PDF, args, c, GT=True):
+    """
+    This is the function to calculate the probability that x is >c or <c depending
+    on the GT boolean.
+    Step 1:  unpack args into mu and stDev
+    Step 2:  compute lhl and rhl for Simpson
+    Step 3:  package new tuple args1=(mu, stDev, lhl, rhl) to be passed to Simpson
+    Step 4:  call Simpson with GNPDF and args1
+    Step 5:  return probability
+    :param PDF: the probability density function to be integrated
+    :param args: a tuple with (mean, standard deviation)
+    :param c: value for which we ask the probability question
+    :param GT: boolean deciding if we want probability x>c (True) or x<c (False)
+    :return: probability value
+    """
     pass
 
 def GPDF(args):
+    """
+    Here is where I will define the Gaussian probability density function.
+    This requires knowing the population mean and standard deviation.
+    To compute the GPDF at any value of x, I just need to compute as stated
+    in the homework assignment.
+    Step 1:  unpack the args tuple into variables called: x, mu, stDev
+    Step 2:  compute GPDF value at x
+    Step 3:  return value
+    :param args: (x, mean, standard deviation)  tuple in that order
+    :return: value of GPDF at the desired x
+    """
     pass
 
-def Simpson(fx, lhl, rhl):
+def Simpson(fx, args):
+    """
+    This executes the Simpson 1/3 rule for numerical integration.  I need
+    to go to the textbook to look up the details.  As I recall:
+    1. divide the range from x=lhl to x=rhl into an even number of parts.
+    2. compute fx at each x value between lhl and rhl
+    3. sum the even and odd values of fx as prescribed
+    4. return the area beneath the function fx
+    :param fx: some function of x to integrate
+    :param args: a tuple containing (mean, stDev, lhl, rhl)
+    :return: the area beneath the function between lhl and rhl
+    """
     pass
 
 def main():
     """
     I want to integrate the Gaussian probability density function between
-    a left hand limit of mean - 5*stDev to a right hand limit of c.
-    :return:
+    a left hand limit = (mean - 5*stDev) to a right hand limit = (c).  Here
+    is my step-by-step plan:
+    1. Decide mean, stDev, and c and if I want P(x>c) or P(x<c).
+    2. Define args tuple and c to be passed to Probability
+    3. Pass args, and a callback function (GPDF) to Probability
+    4. In probability, pass along GPDF to Simpson along with the appropriate args tuple
+    5. Return the required probability from Probability and print to screen.
+    :return: Nothing to return, just print results to screen.
     """
